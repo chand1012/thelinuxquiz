f7f72adf00e1a3af10915fd81ad34df185f3359b
chand1012 <chandler@chand1012.dev>
2021-08-25 17:15:25 -0400

    Add new files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import Grid from '@geist-ui/react/Grid';
+
+const NavBar = (props) => {
+    return (
+        <Grid.Container gap={2} justify="left">
+            <Grid xs={12}>
+                
+            </Grid>
+        </Grid.Container>
+    );
+};
+
+export default NavBar;

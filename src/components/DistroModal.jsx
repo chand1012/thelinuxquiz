a02c93c28d59fd1b49dd9927b0d192f4276c2ade
chand1012 <chandler@chand1012.dev>
2021-08-29 11:17:00 -0400

    Add initial file for results modal

---
+++ src/components/DistroModal.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+// import { Modal } from "@geist-ui/react";
+
+// // this will be the modal
+// // that shows the results of the
+// // quix
+// const DistroModal = (props) => {};
+
+// export default DistroModal;

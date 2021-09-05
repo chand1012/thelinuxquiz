4019493bab4fc37fd68614ce9327ce85629851df
chand1012 <chandler@chand1012.dev>
2021-09-04 22:06:13 -0400

    Add spacers and change layout type

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import { Button, Divider, Grid, Link } from "@geist-ui/react";
+import { Button, Divider, Grid, Link, Spacer } from "@geist-ui/react";
 import { Moon, Sun } from "@geist-ui/react-icons";
 
-// this just needs completely redone
+// the links are not centered
+// can someone who knows what they're doing help me out?
 
 const NavBar = ({ children, themeSwitcher, themeType }) => (
   <header>
-    <Grid.Container justify="center" alignContent="stretch">
-      <Grid>
-        <Link>
-          <Button hoverable>
-            <h4> The Linux Quiz </h4>
+    <Spacer h={1} />
+    <div style={{ marginRight: 10, marginLeft: 10 }}>
+      <Grid.Container justify="space-between">
+        <Grid>
+          <Link>
+            <Button hoverable>
+              <h4> The Linux Quiz </h4>
+            </Button>
+          </Link>
+        </Grid>
+        <Grid>{children}</Grid>
+        <Grid>
+          <Button style={{}} auto onClick={themeSwitcher}>
+            <div style={{ marginTop: 6 }}>
+              {themeType === "light" ? <Moon /> : <Sun />}
+            </div>
           </Button>
-        </Link>
-      </Grid>
-      <Grid>{children}</Grid>
-      <Grid>
-        <Button auto onClick={themeSwitcher}>
-          <div style={{ marginTop: 6 }}>
-            {themeType === "light" ? <Moon /> : <Sun />}
-          </div>
-        </Button>
-      </Grid>
-    </Grid.Container>
+        </Grid>
+      </Grid.Container>
+    </div>
     <Divider />
   </header>
 );
